5e52a755e8ac8c86955e5c0a1942043163ead07c
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-07-18 02:04:59 +0530

    origin

--- sections/Video.tsx
+++ sections/Video.tsx
@@ -25,7 +25,7 @@ const Video = () => {
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          
         ></iframe>
       </motion.div>
     </div>
