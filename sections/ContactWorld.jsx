7424ce66d4814d6316d88021cb5b773a607f207e
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-09-12 18:12:56 +0530

    origin

--- sections/ContactWorld.jsx
+++ sections/ContactWorld.jsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 
 const ContactWorld = () => (
-  <section className= {`${styles.paddings} md:h-[70vh] h-50vh relative `}>
+  <section className= {`${styles.paddings} md:h-[120vh] h-50vh relative `}>
     <motion.div
       variants={staggerContainer}
       initial="hidden"
