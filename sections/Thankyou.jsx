57f5704cd576e5f58052b4618a8ad6c9cdbf0539
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-09-27 10:41:37 +0530

    origin

--- sections/Thankyou.jsx
+++ sections/Thankyou.jsx
@@ -1,17 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+
 
 
 const Thankyou = () => {
   return (
-    <div className="h-[60vh] flex flex-col justify-center items-center">
+    <div className="h-[60vh] md:h-[85vh] lg:pb-[110px] lg:pt-32 flex flex-col justify-center items-center bg-[#f3f7f8] pt-40">
 
-    <h2 className="  text-4xl my-8 ">
-Thank You
-
-    </h2>
-
+        <div>
+        <Image
+            src="/thankyou.jpg"
+            width={800}
+            height={600}
+            alt="Thank You"
+           
+            className=" lg:w-[1100px] lg:h-auto  rounded-lg z-10"
+          />
+        </div>
+   <div className=" flex flex-col justify-center items-center z-20 lg:mt-[-80px]">    
+ <p className=" flex justify-center items-center text-center px-8 text-xl lg:text-3xl font-bold">Thank you for reaching out. We will get in touch with you soon.</p>
+ <Link href={'/'}>
+<div className=" mb-32 mt-14">
 <button className=" bg-pink-600 py-2 px-6 text-white font-bold rounded-md"> Back to Home</button>
-
-
+</div>
+</Link>
+</div> 
     </div>
   )
 }
