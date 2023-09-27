9f9c2a0054c88b989af6fbb4c828073bceede888
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-09-27 09:43:17 +0530

    origin

---
+++ pages/thank-you/index.js
@@ -0,0 +1,22 @@
+import { Footer, Navbar } from "../../components"
+import { Thankyou } from "../../sections"
+
+
+const thankYou = () => {
+  return (
+    <>
+
+    <div>
+
+        <Navbar/>
+        <Thankyou/>
+        <Footer/>
+    </div>
+
+    </>
+    
+    
+  )
+}
+
+export default thankYou
---
+++ sections/Thankyou.jsx
@@ -0,0 +1,19 @@
+
+
+const Thankyou = () => {
+  return (
+    <div className="h-[60vh] flex flex-col justify-center items-center">
+
+    <h2 className="  text-4xl my-8 ">
+Thank You
+
+    </h2>
+
+<button className=" bg-pink-600 py-2 px-6 text-white font-bold rounded-md"> Back to Home</button>
+
+
+    </div>
+  )
+}
+
+export default Thankyou
