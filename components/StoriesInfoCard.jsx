47de025231746ffbb281e5468d9d1b4a9fe59845
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-12-22 19:20:25 +0530

    origin

--- components/StoriesInfoCard.jsx
+++ components/StoriesInfoCard.jsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 const StoriesInfoCard = () => {
   return (
-    <div className=" lg:h-[220vh] md:h-[400vh] md:mx-16 lg:mx-40 ">
+    <div className=" lg:h-[280vh] md:h-[400vh] md:mx-16 lg:mx-40 ">
       {/* Story one */}
       <div className=" flex gap-12 justify-center">
         <Card className="w-full items-center p-24 max-w-[48rem] flex-row">
@@ -109,7 +109,7 @@ const StoriesInfoCard = () => {
        
       </div>
       {/* Story Three */}
-      <div className=" flex gap-12 mt-24 justify-center mb-52">
+      <div className=" flex gap-12 mt-24 justify-center mb-[800px]">
         <Card className="w-full items-center p-24 max-w-[48rem] flex-row">
           <CardBody>
             <Typography variant="h6" color="gray" className="mb-4 text-7xl uppercase">
