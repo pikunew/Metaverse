9c80aa7f0df104d9fd52ec3cc71a58a5b90f84c9
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-12-22 16:40:43 +0530

    origin

--- components/StoriesCard.jsx
+++ components/StoriesCard.jsx
@@ -26,7 +26,7 @@ const StoriesCard = () => {
               Some Of the real stories 
             </Typography>
             <Typography variant="lead" className="mb-10 mt-6 !text-gray-900">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </Typography>
             <div className="mb-8 flex justify-center gap-4 lg:justify-start">
               <Button className=" bg-[#BE185D]">view all stories</Button>
